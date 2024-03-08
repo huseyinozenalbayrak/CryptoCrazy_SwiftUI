00fae374f45ed5f9a381988718d20054183417f6
huseyinozenalbayrak <albayrakhuseyinozen@gmail.com>
2024-03-08 13:58:25 +0300

    open to improvement

--- CryptoCrazySwiftUI/Service/Webservice.swift
+++ CryptoCrazySwiftUI/Service/Webservice.swift
@@ -17,6 +17,7 @@ class Webservice {
     }
      */
     
+    
     func downloadCurrenciesContinuation(url: URL) async throws -> [CryptoCurrency] {
         try await withCheckedThrowingContinuation({ continuation in
             downloadCurrencies(url: url) { result in
